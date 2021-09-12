052de025a721327c7e7ab489c7796a0066366eac
RTPablocs <conejerosorianopablo@gmail.com>
2021-09-12 19:09:57 +0200

    Added base route for users

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/user.js')
+
+
+router.get('/', userController.listAllUsers)
 
 router.get('/:id', (req, res) => {
     res.json('')
